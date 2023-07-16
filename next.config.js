2376425003e54df841e4cd2682254f08e6ff3424
nhonnq <nhonnq.dev@gmail.com>
2023-07-16 11:08:22 +0700

    Update github deploy configure

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const repo = 'nhonnq.github.io'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
-
-const nextConfig = {
-    assetPrefix: assetPrefix,
-    basePath: basePath
-}
+const nextConfig = {}
 
 module.exports = nextConfig
